40f8ccb20065784825401f43eefa468b39fd2540
Vbj <ijalab1808@gmail.com>
2020-05-24 21:13:37 +0530

    ADDED STYLE

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const api = {
+  key: "86aa98cfeffa6f6779d9ece3a9d4044d" ,
+  base: "https://api.openweathermap.org/data/2.5/"
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            />
+        </div>
+      </main>
     </div>
   );
 }
